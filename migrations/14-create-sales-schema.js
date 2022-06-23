f299ac8ee06e897b3430c051a830db3514a8e3fd
Xavier-Labarbe <xavier.labarbe@outlook.fr>
2022-06-23 16:14:33 +0200

    removing schemas

--- migrations/14-create-sales-schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createSchema("sales");
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropSchema("sales");
-  },
-};
--- migrations/16-create-dev-schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createSchema("dev");
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropSchema("dev");
-  },
-};
